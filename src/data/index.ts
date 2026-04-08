c42b0122be7b04c97784ad69259f95b6d5294965
Z User <z@container>
2026-04-08 12:30:02 +0000

    v3.404: Fix geography tests for 11th grade - add import, rewrite 12 games to match lesson names

--- src/data/index.ts
+++ src/data/index.ts
@@ -344,6 +344,7 @@ import { games as grade11ChemistryGames } from './grades/11/chemistry'
 import { games as grade11HistoryGames } from './grades/11/history'
 import { games as grade11SocialGames } from './grades/11/social'
 import { games as grade11BiologyGames } from './grades/11/biology'
+import { games as grade11GeographyGames } from './grades/11/geography'
 
 // Старые игры из файлов grade-*.ts
 import { prepClassGames } from './games/grade-0'
@@ -419,7 +420,7 @@ export const allGames: Record<number, GameLesson[]> = {
   8: [...eighthGradeGames, ...grade8PhysicsGames, ...grade8CodingGames, ...grade8SocialGames, ...grade8EconomyGames, ...grade8InformaticsGames, ...grade8RoboticsGames, ...grade8LawGames],
   9: [...ninthGradeGames, ...grade9AlgebraGames, ...grade9GeometryGames, ...grade9PhysicsGames, ...grade9ChemistryGames, ...grade9BiologyGames, ...grade9GeographyGames, ...grade9HistoryGames, ...grade9RussianGames, ...grade9LiteratureGames, ...grade9SocialGames, ...grade9CodingGames, ...grade9InformaticsGames, ...grade9PsychologyGames, ...grade9EconomyGames, ...grade9CareerGames, ...grade9OGEGames],
   10: [...tenthGradeGames, ...grade10EconomyGames, ...grade10CodingGames, ...grade10TechGames, ...grade10ArtGames, ...grade10CareerGames, ...grade10AstronomyGames, ...grade10BusinessGames, ...grade10PhilosophyGames, ...grade10ProjectsGames, ...grade10LabGames, ...grade10EGEGames],
-  11: [...eleventhGradeGames, ...grade11EconomyGames, ...grade11CodingGames, ...grade11LiteratureGames, ...grade11RussianGames, ...grade11AlgebraGames, ...grade11GeometryGames, ...grade11PhysicsGames, ...grade11ChemistryGames, ...grade11HistoryGames, ...grade11SocialGames, ...grade11BiologyGames],
+  11: [...eleventhGradeGames, ...grade11EconomyGames, ...grade11CodingGames, ...grade11LiteratureGames, ...grade11RussianGames, ...grade11AlgebraGames, ...grade11GeometryGames, ...grade11PhysicsGames, ...grade11ChemistryGames, ...grade11HistoryGames, ...grade11SocialGames, ...grade11BiologyGames, ...grade11GeographyGames],
 }
 
 // Curriculum данные
